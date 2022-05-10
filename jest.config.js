6549d70fc0c57026f9a92a7cb85c9453eb11ea4e
shrine <shrine8263@yeah.net>
2022-05-11 05:46:05 +0800

    chore: add config files

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "forceExit": true,
+  "setupFilesAfterEnv": [
+    "<rootDir>/test/setupTest.js"
+  ],
+  "testMatch": [
+    "<rootDir>/test/*.test.js",
+    "<rootDir>/test/*.unittest.js"
+  ],
+  "watchPathIgnorePatterns": [
+    "<rootDir>/.git",
+    "<rootDir>/node_modules",
+    "<rootDir>/examples/*/dist",
+    "<rootDir>/coverage",
+  ],
+  "modulePathIgnorePatterns": [
+    "<rootDir>/.git"
+  ],
+  "transformIgnorePatterns": [
+    "<rootDir>"
+  ],
+  "coverageDirectory": "<rootDir>/coverage",
+  "coveragePathIgnorePatterns": [
+    "<rootDir>/test",
+    "<rootDir>/example",
+    "<rootDir>/node_modules"
+  ],
+  "testEnvironment": "node",
+  "coverageReporters": ["clover", "json", "lcov", "text"]
+}
